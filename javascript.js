6ba2fda9c4e4d15baf652be34d1dfd8a3f8fa217
Mario D <manriqueduran123@gmail.com>
2022-06-22 23:19:56 -0400

    added sequence of else if to check for paper and scissor

--- javascript.js
+++ javascript.js
@@ -1,30 +1,50 @@
-
-
-
 //make func to play game
 function rpc() {
 
     // function to randomize number
     function getRan() {
-        return Math.floor(Math.random() * 3);
+        return Math.floor(Math.random() * 2);
     }
 
     //get user input
     let input = prompt("Choose, rock-paper-scissor... GO!");
     input = input.toLowerCase();
-    
+
+    let answer =  "You picked " + input + "!...";
+
     // if and else if to check for possibilities
     if (input == "rock") {
-        let answer =  "You picked " + input + "!...";
         if (getRan() == 0) {
-            return answer + "computer says 'rock'..." + "Draw!";
+            return answer + "Computer says 'rock'..." + "Draw!";
         } else if (getRan() == 1) {
-            return answer + "computer says 'paper'..." + "sorry you lost";
+            return answer + "Computer says 'paper'..." + "Sorry you lost";
         } else if (getRan() == 2) {
-            return answer + "computer says 'scissor'..." + "sorry you lost";
+            return answer + "computer says 'scissor'..." + "Congratulations, You Won!";
         } else {
             return "Error";
         }
+    } else if (input == "paper") {
+        if (getRan() == 0) {
+            return answer + "Computer says 'rock'..." + "Congratulations, You Won!";
+        } else if (getRan() == 1) {
+            return answer + "Computer says 'paper'..." + "Draw!";
+        } else if (getRan() == 2) {
+            return answer + "computer says 'scissor'..." + "Sorry you lost";
+        } else {
+            return "Error";
+        }
+    } else if (input == "scissor") {
+        if (getRan() == 0) {
+            return answer + "Computer says 'rock'..." + "Sorry you lost";
+        } else if (getRan() == 1) {
+            return answer + "Computer says 'paper'..." + "Congratulations, You Won!";
+        } else if (getRan() == 2) {
+            return answer + "computer says 'scissor'..." + "Draw!";
+        } else {
+            return "Error";
+        }
+    } else {
+        return "sorry, valid inputs are 'rock, paper, scissor'";
     }
 }
 
