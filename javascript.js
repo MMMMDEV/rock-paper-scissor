58c09f6cb588c7e52479dd0af93b2876cec55d1d
Mario D <manriqueduran123@gmail.com>
2022-06-22 22:56:27 -0400

    added function to play to play the game and i made a function to accept a string from user and return a simple won or lost scenerio using the random number

--- javascript.js
+++ javascript.js
@@ -1,7 +1,31 @@
-// function to randomize number
 
-function getRan(num) {
-    return Math.floor(Math.random() * num);
+
+
+//make func to play game
+function rpc() {
+
+    // function to randomize number
+    function getRan() {
+        return Math.floor(Math.random() * 3);
+    }
+
+    //get user input
+    let input = prompt("Choose, rock-paper-scissor... GO!");
+    input = input.toLowerCase();
+    
+    // if and else if to check for possibilities
+    if (input == "rock") {
+        let answer =  "You picked " + input + "!...";
+        if (getRan() == 0) {
+            return answer + "computer says 'rock'..." + "Congratulations! you won";
+        } else if (getRan() == 1) {
+            return answer + "computer says 'paper'..." + "sorry you lost";
+        } else if (getRan() == 2) {
+            return answer + "computer says 'scissor'..." + "sorry you lost";
+        }
+    }
 }
 
-console.log(getRan(3));
+
+
+console.log(rpc());
